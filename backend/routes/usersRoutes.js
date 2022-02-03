9b5aee3fee76074def15f181063c159fce2dd593
Paul Devlin <pmdevlin360@gmail.com>
2022-02-03 16:48:19 -0600

    removed .thens and created responses for each request

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,8 +1,9 @@
-import {Router} from 'express'
+import {Router, json, application} from 'express'
 import db from '../db/index.js'
 
 
 const users = new Router()
+users.use(json())
 
 users.get('/', async (req, res) => {
   try{
@@ -27,25 +28,21 @@ users.get('/:id', async (req, res) => {
 
 users.post('/', async (req, res) => {
     try{
-        const result = await db('users').insert({
-            "first_name": req.body.first_name,
-            "last_name": req.body.last_name,
-            "email": req.body.email,
-            "password": req.body.password
-        }).then(() => res.json({message: 'add a user'}))
+        const result = await db('users').insert(req.body).returning('*')
+        res.json(result)
     }catch(err){
         errorHandler(err, res)
     }
 })
 
-users.put('/:id', async (req, res) => { 
+users.patch('/:id', async (req, res) => { 
     try{
         const id = req.params.id
-         await db('users')
+        const result = await db('users')
         .where({id: id})
         .update(req.body)
         .returning('*')
-        .then((data) => res.send(data))
+        res.json(result)
 
     }catch(err){
         errorHandler(err, res)
@@ -58,7 +55,7 @@ users.delete('/:id', async (req, res) => {
         await db('users')
         .where({id: id})
         .del()
-        .then(() => res.json({message: `deleted user a id ${id}`}))
+        res.json({message: `deleted user a id ${id}`})
 
     }catch(err){
         errorHandler(err, res)
