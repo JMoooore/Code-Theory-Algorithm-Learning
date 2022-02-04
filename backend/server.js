428b0e1cf517ba40098825cdf2e508707a187c8b
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-02-04 11:33:01 -0500

    Added connection.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,18 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import cors from 'cors'
-import express from 'express'
-import * as routes from './routes/index.js'
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import * as routes from './routes/index.js';
 
+console.log(process.env);
 
-const app = express()
+const app = express();
 const corsConfig = {
-    origin: 'http://localhost:3000'
-}
-app.use(cors(corsConfig))
-const PORT = process.env.PORT || 3001
-app.use(express.json())
+    origin: 'http://localhost:3000',
+};
+app.use(cors(corsConfig));
+const PORT = process.env.PORT || 3001;
+app.use(express.json());
 
-
-
-app.listen(PORT, ()=>{
-    console.log(`server running on port: ${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`server running on port: ${PORT}`);
+});
