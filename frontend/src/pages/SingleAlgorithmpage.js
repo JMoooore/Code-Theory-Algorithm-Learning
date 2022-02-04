b1e4580e5098503207b7e9dafb83e0c0c1cf9146
IsaiasNG <isaiasnunogalindo@gmail.com>
2022-02-04 16:57:26 -0600

    updates to SingleAlgopage for description

--- frontend/src/pages/SingleAlgorithmpage.js
+++ frontend/src/pages/SingleAlgorithmpage.js
@@ -1,44 +1,43 @@
 import React from 'react';
 import styles from '../styles/singleAlgorithmpage.module.css'
 
-export default function SingleAlgorithmpage() {
+export default function SingleAlgorithmpage({algorithm}) {
   return (
     <>
     <div className={styles.mainContainer}>
         <div className={styles.animationContainer}>
             <div className={styles.animationCard}>
-                <div className={styles.animationItem}>Animation</div>
+                <div className={styles.animationItem}></div>
                 <div className={styles.animationControls}></div>
             </div>
         </div>
         <div className={styles.descriptionContainer}>
             <div className={styles.descriptionCard}>
                 <div className={styles.descriptionTitle}>
-                <h1>Bubble Sort algorithm using JavaScript</h1>
+                {/* <h1>{algorithm.title}</h1> */}
                 </div>
                 <div className={styles.descriptionBody}>
-                    <p>Bubble sort algorithm is an algorithm that sorts the array by comparing two adjacent elements and swaps them if they are not in the intended order. Here order can be anything like increasing order or decreasing order.
-
-                    How Bubble-sort works
-                    We have an unsorted array arr = [ 1, 4, 2, 5, -2, 3 ] the task is to sort the array using bubble sort. 
-
-                    Bubble sort compares the element from index 0 and if the 0th index is less than 1st index then the values get swapped and if the 0th index is less than the 1st index then nothing happens.
-
-                    then, the 1st index compares to the 2nd index then the 2nd index compares to the 3rd, and so on…</p>
+                    {/* <p>{algorithm.description}</p> */}
                 </div>
             </div>
         </div>
             
         <div className={styles.codeContainer}>
-            <div className={styles.codeCard}>Code</div>
+            <div className={styles.codeCard}>
+                <div></div>
+            </div>
         </div>
             
         <div className={styles.commentsContainer}>
-            <div className={styles.commentsCard}>Comments</div>
+            <div className={styles.commentsCard}>
+                <div></div>
+            </div>
         </div>
 
         <div className={styles.nextAlgoContainer}>
-            <div className={styles.nextAlgoCard}>Next Algo</div>
+            <div className={styles.nextAlgoCard}>
+                <div></div>
+            </div>
         </div>
     </div>
     </>
