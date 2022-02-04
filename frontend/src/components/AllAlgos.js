e505ff9c0c294c2ce9ca88086fbbb8837bcbb436
fernandocm9 <88690826+fernandocm9@users.noreply.github.com>
2022-02-04 11:43:29 -0600

    set up foundation for the cards

---
+++ frontend/src/components/AllAlgos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/allAlgos.module.css'
+
+export default function AllAlgos() {
+  return (
+      <>
+        <div className={styles.backgroundAllAlgos}>
+        {/* <AlgoCard/> */}
+        </div>
+      </>  
+  )
+}
