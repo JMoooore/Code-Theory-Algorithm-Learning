6ae934cbcc114b152826080c2517076fc257b689
fernandocm9 <88690826+fernandocm9@users.noreply.github.com>
2022-02-04 17:24:59 -0600

    imported AlgoCard to AllAlgos

--- frontend/src/components/AllAlgos.js
+++ frontend/src/components/AllAlgos.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from '../styles/allAlgos.module.css'
+import AlgoCard from './AlgoCard';
 
 export default function AllAlgos() {
   return (
       <>
         <div className={styles.backgroundAllAlgos}>
-        {/* <AlgoCard/> */}
+        <AlgoCard/>
         </div>
       </>  
   )
