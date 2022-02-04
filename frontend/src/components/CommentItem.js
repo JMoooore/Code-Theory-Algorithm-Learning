3beaba0ac1dd37361631e6b39819c46ba3c22061
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-02-04 16:42:00 -0500

    Comment container and CommentItem layed out. Rough draft

--- frontend/src/components/CommentItem.js
+++ frontend/src/components/CommentItem.js
@@ -1,10 +1,14 @@
 import React from "react"
 import styles from '../styles/comment.module.css'
 
+// props to bring in -> user.id, user.first_name, user.last_name[0], comment.content, comment.created_at, comment.id
+
 export default function CommentItem(props) {
   return (
-    <>
+    <div className={styles.commentItem}>
       <div className={styles.commentPhoto}></div>
-    </>
+      <div className={styles.commentName}> Mike Jones </div>
+      <div className={styles.commentText}>This is such a great algorithm</div>
+    </div>
   )
 }
