0fdc01b845d865dd6c4550358a69ff14abba584d
John Moore <johnhtmoore@gmail.com>
2022-02-04 18:04:01 -0700

    added cascading deletion for migration file and added rollback script

--- backend/db/migrations/20220203190856_comments.js
+++ backend/db/migrations/20220203190856_comments.js
@@ -6,7 +6,7 @@ export function up(knex) {
   return knex.schema.createTable("comments", (table) => {
     table.increments("id").primary(); // unique
     table.integer("user_id");
-    table.foreign("user_id").references("users.id");
+    table.foreign("user_id").references("users.id").onDelete("CASCADE");
     table.string("content");
     table.timestamp("created_at").defaultTo(knex.fn.now());
   });
--- backend/db/seeds/20220204162330_seed_users.js
+++ backend/db/seeds/20220204162330_seed_users.js
@@ -3,6 +3,7 @@
  * @returns { Promise<void> }
  */
 export function seed(knex) {
+  
   // Deletes ALL existing entries
   return knex("users")
     .del()
@@ -10,14 +11,12 @@ export function seed(knex) {
       // Inserts seed entries
       return knex("users").insert([
         {
-          id: 1,
           first_name: "paul",
           last_name: "devlin",
           email: "pd@email.com",
           password: "1234",
         },
         {
-          id: 2,
           first_name: "kevin",
           last_name: "heleodoro",
           email: "kh@email.com",
