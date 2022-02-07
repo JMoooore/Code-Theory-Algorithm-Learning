0ee9651c98e38d3474271caa34a9a7ecf191e280
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-02-07 14:49:38 -0500

    Comment submit button and textarea in Comment Component

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -3,17 +3,28 @@ import styles from '../styles/comment.module.css'
 import CommentItem from './CommentItem';
 
 export default function Comment(props){
+  function clickHandler(e){
+    // Route this to a POST request through App
+    e.preventDefault()
+    console.log(e.target);
+  }
+
   return (
     <ul className={styles.commentContainer}>
       <h1 className={styles.commentTitle}>Comments</h1>
-      <form className={styles.comment}>
-        <textarea></textarea>
+      {/* Add comment form should only render if user is logged in, 
+      else it should prompt user to login */}
+      <form className={styles.commentForm}>
+        <textarea className={styles.addCommentTextarea}></textarea>
+        <button 
+          className={styles.addCommentButton}
+          onClick={(e) => clickHandler(e)}
+        >Submit Comment</button>
       </form>
       {props.comments.map(e => {
-        return <CommentItem comment={e} user={props.users.filter((a) => {
+        return <CommentItem key={e.id} comment={e} user={props.users.filter((a) => {
           return a.id === e.user_id ? a : null;
-          }
-          )}/>
+          })}/>
       })}
     </ul>
   )
--- frontend/src/components/CommentItem.js
+++ frontend/src/components/CommentItem.js
@@ -2,7 +2,6 @@ import React from "react"
 import styles from '../styles/comment.module.css'
 
 export default function CommentItem(props) {
-
   return (
     <li className={styles.commentItem}>
         <div className={styles.commentPhotoContainer}>
