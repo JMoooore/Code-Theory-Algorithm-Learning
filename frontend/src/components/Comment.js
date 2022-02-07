bd55ed257c4eb0fba585db62270288225a50efa2
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-02-07 09:39:58 -0500

    Date object needs formatting. CommentItem text needs to be pushed to the right.

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -2,8 +2,6 @@ import React from 'react';
 import styles from '../styles/comment.module.css'
 import CommentItem from './CommentItem';
 
-// props to bring in -> user.id, user.first_name, user.last_name[0], comment.content, comment.created_at, comment.id
-
 export default function Comment(props){
   return (
     <ul className={styles.commentContainer}>
--- frontend/src/components/CommentItem.js
+++ frontend/src/components/CommentItem.js
@@ -5,26 +5,14 @@ export default function CommentItem(props) {
 
   return (
     <li className={styles.commentItem}>
-    {console.log(props.user)}
         <div className={styles.commentPhotoContainer}>
             <img className={styles.commentPhoto} alt=''/> {/*props.user.user_photo */}
         </div>
         <div className={styles.commentInfo}>
-            <p className={styles.commentName}>{props.user.first_name, props.user.last_name}</p>
+            <p className={styles.commentName}>{props.user[0].first_name} {props.user[0].last_name[0]}.</p>
             <span className={styles.commentTime}>{props.comment.created_at}</span>
         </div>
         <p className={styles.commentText}>{props.comment.content}</p>
     </li>
   )
 }
-
-
-
-// <div className={styles.commentPhotoContainer}>
-//             <img className={styles.commentPhoto} alt=''/> {/*props.user.user_photo */}
-//         </div>
-//         <div className={styles.commentInfo}>
-//             <p className={styles.commentName}>Anie Silverston {/* props.user.first_name, props.user.last_name[0] */}</p>
-//             <span className={styles.commentTime}>4 hours ago {/* props.comment.created_at */}</span>
-//         </div>
-//         <p className={styles.commentText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel dolor tincidunt, hendrerit enim at, imperdiet dui. Nullam porta eleifend bibendum. Sed ultricies convallis fermentum. Cras quam augue, auctor at faucibus at, tincidunt et massa. Morbi viverra augue nunc, a dignissim elit facilisis mattis. Aliquam ut dignissim nunc. Pellentesque sed lobortis dolor. Nulla ut ante volutpat sapien efficitur condimentum. Aenean volutpat gravida cursus. Cras sit amet lobortis augue. Phasellus luctus, nunc in ullamcorper iaculis, purus mauris imperdiet enim, vel euismod lorem turpis vel ligula. Sed id nisi sodales, ullamcorper tellus vel, laoreet purus. Donec tempor semper arcu, id tempus neque sagittis finibus. Phasellus viverra metus sed nisl efficitur euismod. Nam eu mi vehicula, cursus leo sit amet, eleifend mauris. {/* props.comment.content */}</p>
