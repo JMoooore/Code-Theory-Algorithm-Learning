1551c34020dd28efbec12fec4250c1c563f0d914
IsaiasNG <isaiasnunogalindo@gmail.com>
2022-02-09 17:46:42 -0600

    added replitCode prop for the code blocks in each algo

--- frontend/src/algos/insertionSort.js
+++ frontend/src/algos/insertionSort.js
@@ -4,6 +4,8 @@ const algo = {}
 
 algo.name = 'Insertion Sort'
 
+algo.replitCode = "https://replit.com/@EmpatheticLions/Insertion-Sort?embed=true";
+
 algo.description = 'Insertion sort is a simple sorting algorithm that works similar to the way you sort playing cards in your hands. The array is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part.'
 
 algo.image = insertionImage
