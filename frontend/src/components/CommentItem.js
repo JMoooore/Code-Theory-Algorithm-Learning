530803f9b9e629503fac60d15c8db7efb49116f9
IsaiasNG <isaiasnunogalindo@gmail.com>
2022-02-09 17:08:52 -0600

    added code block iframe, updated backend migrate and seed, updated comments, and singlpage

--- frontend/src/components/CommentItem.js
+++ frontend/src/components/CommentItem.js
@@ -2,18 +2,23 @@ import React from "react"
 import styles from '../styles/comment.module.css'
 
 export default function CommentItem(props) {
+  const {first_name, last_name, content, created_at} = props.comment
+
+  const timeDifference = () => {
+    let timeFromDB = new Date(created_at)
+    return timeFromDB.toLocaleDateString()
+  }
 
   return (
     <li className={styles.commentItem}>
-    {console.log(props.users)}
         <div className={styles.commentPhotoContainer}>
-            <img className={styles.commentPhoto} alt=''/> {/*props.user.user_photo */}
+            <img className={styles.commentPhoto} alt=''/> {/*props.comment.user_photo */}
         </div>
         <div className={styles.commentInfo}>
-            <p className={styles.commentName}>{props.user[0].first_name} {props.user[0].last_name[0]}.</p>
-            <span className={styles.commentTime}>{props.comment.created_at}</span>
+            <p className={styles.commentName}>{first_name} {last_name[0]}.</p>
+            <span className={styles.commentTime}>{timeDifference()}</span>
         </div>
-        <p className={styles.commentText}>{props.comment.content}</p>
+        <p className={styles.commentText}>{content}</p>
     </li>
   )
 }
--- frontend/src/pages/SingleAlgorithmPage.js
+++ frontend/src/pages/SingleAlgorithmPage.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from '../styles/singleAlgorithmPage.module.css'
+import Comment from '../components/Comment';
+import axios from 'axios';
 
 export default function SingleAlgorithmPage(props) {
+  const [comments, setComments] = useState()
   const algorithm = props.singleAlgo
+
+  const getCommentData = () => {
+    axios.get(`http://localhost:3001/comments/${algorithm.name}`)
+    .then(res => {setComments(res.data)})
+    .catch(console.log('whoops'))
+  }
+
+  useEffect(() => getCommentData(),[])
+
   return (
     <>
     <div className={styles.mainContainer}>
@@ -25,14 +37,12 @@ export default function SingleAlgorithmPage(props) {
             
         <div className={styles.codeContainer}>
             <div className={styles.codeCard}>
-                <div></div>
+                <iframe frameBorder="0" width="100%" height="100%" src={algorithm.replitCode}></iframe>
             </div>
         </div>
             
         <div className={styles.commentsContainer}>
-            <div className={styles.commentsCard}>
-                <div></div>
-            </div>
+            {comments && <Comment comments={comments} currentAlgo={algorithm.name} getCommentData={getCommentData}/>}
         </div>
 
         <div className={styles.nextAlgoContainer}>
