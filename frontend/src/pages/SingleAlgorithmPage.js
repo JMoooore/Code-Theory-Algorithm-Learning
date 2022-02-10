252ceb4ad7eb86880a6d5d2acd2b4557a26d5bda
Paul Devlin <pmdevlin360@gmail.com>
2022-02-10 17:45:54 -0600

    added logic for link path name needs to be entered to the link when other algos are completed

--- frontend/src/pages/SingleAlgorithmPage.js
+++ frontend/src/pages/SingleAlgorithmPage.js
@@ -1,13 +1,24 @@
 import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom'; 
 import styles from '../styles/singleAlgorithmPage.module.css';
 import Comment from '../components/Comment';
 import axios from 'axios';
 import SortingVisualizer from '../components/SortingVisualizer/index';
 import { Grid, Row, Col } from '../components/GridRow';
+import algoObj from '../algos/index'
 
 export default function SingleAlgorithmPage(props) {
   const [comments, setComments] = useState()
   const {algorithm} = props
+   console.log(algoObj)
+
+  const next = (obj) => {
+      for(let key in obj){
+          if(key !== algorithm.routeName){
+              return obj[key].image
+          }
+      }
+  }
 
   const getCommentData = () => {
     axios.get(`http://localhost:3001/comments/${algorithm.name}`)
@@ -50,7 +61,9 @@ export default function SingleAlgorithmPage(props) {
                       <p>{algorithm.description}</p>
                   </div>
               <div className={styles.nextAlgoContainer}>
-                <div className={styles.nextAlgoCard}>NEXT ALGO CARD</div>
+                <div className={styles.nextAlgoCard}>
+                <Link to={`/algorithms`}><img className={styles.NextImg} src={next(algoObj)} alt=''/></Link>
+                </div>
               </div>
               </div>
           </div>
