a0bddf4d3bc816dd3c6b0bf76b17ea109581c7ec
IsaiasNG <isaiasnunogalindo@gmail.com>
2022-02-10 15:47:28 -0600

    updated header and footer to render for multi platforms and a bit of styling

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -9,20 +9,20 @@ export default function Footer() {
             <div className={styles.footerLogo}>
                 <Link to='/'><div className={styles.logoImage}></div></Link> 
                 <div className={styles.authorsDiv}>
-                <div className={styles.authorsTitle}>Authors</div>
-                <div className={styles.authorsListDiv}>
-                    <ul className={styles.footerAuthorList}>
-                        <li className={styles.footerAuthorItem}><a href="https://www.linkedin.com/in/isaias-nuno-galindo/" target='_blank' rel='noopener noreferrer' >Isaias</a></li>
-                        <li className={styles.footerAuthorItem}><a href="https://www.linkedin.com/in/fernando-curiel-moysen/" target='_blank' rel='noopener noreferrer' >Fernando</a></li>
-                        <li className={styles.footerAuthorItem}><a href="https://www.linkedin.com/in/kevin-heleodoro/" target='_blank' rel='noopener noreferrer' >Kevin</a></li>
-                    </ul>
-                    <ul className={styles.footerAuthorList}>
-                        <li className={styles.footerAuthorItem}><a href="https://www.linkedin.com/in/pmdevlin/" target='_blank' rel='noopener noreferrer' >Paul</a></li>
-                        <li className={styles.footerAuthorItem}><a href="https://www.linkedin.com/in/nick-raffaele/" target='_blank' rel='noopener noreferrer' >Nick</a></li>
-                        <li className={styles.footerAuthorItem}><a href="https://www.linkedin.com/in/john-ht-moore/" target='_blank' rel='noopener noreferrer' >John</a></li>
-                    </ul>
+                    <div className={styles.authorsTitle}>Authors</div>
+                    <div className={styles.authorsListDiv}>
+                        <ul className={styles.footerAuthorList}>
+                            <li className={styles.footerAuthorItem}><a href="https://www.linkedin.com/in/isaias-nuno-galindo/" target='_blank' rel='noopener noreferrer' >Isaias</a></li>
+                            <li className={styles.footerAuthorItem}><a href="https://www.linkedin.com/in/fernando-curiel-moysen/" target='_blank' rel='noopener noreferrer' >Fernando</a></li>
+                            <li className={styles.footerAuthorItem}><a href="https://www.linkedin.com/in/kevin-heleodoro/" target='_blank' rel='noopener noreferrer' >Kevin</a></li>
+                        </ul>
+                        <ul className={styles.footerAuthorList}>
+                            <li className={styles.footerAuthorItem}><a href="https://www.linkedin.com/in/pmdevlin/" target='_blank' rel='noopener noreferrer' >Paul</a></li>
+                            <li className={styles.footerAuthorItem}><a href="https://www.linkedin.com/in/nick-raffaele/" target='_blank' rel='noopener noreferrer' >Nick</a></li>
+                            <li className={styles.footerAuthorItem}><a href="https://www.linkedin.com/in/john-ht-moore/" target='_blank' rel='noopener noreferrer' >John</a></li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
             </div>
             
             <div>
