476ed91f866468398598bbae5c2c4d01da8905b7
IsaiasNG <isaiasnunogalindo@gmail.com>
2022-02-10 11:43:31 -0600

    updates

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react';
 import styles from '../styles/loginPage.module.css'; 
+import MainIconImg from '../images/mainicon.svg'
 import { Link, useNavigate } from "react-router-dom";   //useHistory is different for v-6 it is useNavigate
 
 
@@ -33,8 +34,9 @@ export default function LoginPage() {
 
 
             <div className={styles.loginWrapper}>
-                <h1 className={styles.loginTitle}>Login</h1>
-                <form>
+                <img className={styles.loginTitleImg} src={MainIconImg}/>
+                <h1 className={styles.loginTitle}></h1>
+                <form className={styles.loginForm}>
                     <div><input type="text" placeholder='Username' ref={emailRef} autoComplete="new-password"/></div>
                     <div style={{ marginTop: 10 }}><input type="password"placeholder='Password' ref={passwordRef}  autoComplete="new-password"/></div>
                     <button type="submit" className={styles.loginBtn}>Login</button>
