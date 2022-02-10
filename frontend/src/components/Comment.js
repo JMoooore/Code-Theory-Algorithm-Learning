4d90a1864392ebbd66d9ca34f8eeda16625f6393
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-02-10 17:54:52 -0600

    Very ugly submit comment form. It works to send the post request as long as their is a valid user_id entered. Does not currently re-render the Comment component

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -8,9 +8,13 @@ export default function Comment(props){
   const [commentId, setCommentId] = useState('')
 
   function handleSubmit(e){
-    // Route this to a POST request through App
-    // target the text in text area.
-    e.preventDefault()
+    axios.post('http://localhost:3001/comments', {
+      user_id: commentId,
+      content: commentText,
+      algorithm: props.currentAlgo
+    })
+    .then(res => {console.log(res)})
+    .catch((err) => {console.log(err)})
     console.log(`${commentId} said ${commentText}`);
   }
 
@@ -21,18 +25,18 @@ export default function Comment(props){
 
       {/* Add comment form should only render if user is logged in, 
       else it should prompt user to login */}
-      {/* <form className={styles.commentForm}>
+      <form className={styles.commentForm}>
         <input id='newCommentId' className={styles.addCommentId} type="number" placeholder='valid ID' min="1" max="2" onChange={(e) => setCommentId(e.target.value)}></input> 
 
         <textarea id="newCommentContent" className={styles.addCommentTextarea} placeholder="Type your comment..." onChange={(e) => setCommentText(e.target.value)}></textarea>
 
-        <input type="submit" className={styles.addCommentButton}
-          onSubmit={(e) => {
+        <button type="button" className={styles.addCommentButton}
+          onClick={(e) => {
             e.preventDefault()
             handleSubmit(e)
           }}
-        ></input>
-      </form> */}
+        >Submit Comment</button>
+      </form>
       
       {props.comments.map(e => {
         return <CommentItem key={e.id} comment={e} />
