46fbf7430f8fdf3b6e57e7144c32b21161983796
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-02-10 19:25:40 -0500

    Main hotfix round 2

--- frontend/src/algos/quickSort.js
+++ frontend/src/algos/quickSort.js
@@ -1,4 +1,5 @@
 import deepCopy from '../utils/deepCopy.js';
+import quickImage from '../images/quick_img.svg';
 
 
 const SORTED_COLOR = 'orange'
@@ -30,6 +31,8 @@ const algo = {};
 
 algo.name = 'Quick Sort';
 algo.routeName = 'quickSort';
+algo.image = quickImage
+
 
 algo.description =
     'Quicksort is an in-place sorting algorithm. Developed by British computer scientist Tony Hoare in 1959 and published in 1961, it is still a commonly used algorithm for sorting. When implemented well, it can be somewhat faster than merge sort and about two or three times faster than heapsort.';
