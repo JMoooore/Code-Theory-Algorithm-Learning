90d798865b68bc63cbc00a1802c0a970a73c07b3
IsaiasNG <isaiasnunogalindo@gmail.com>
2022-02-11 08:52:53 -0600

    updates

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -9,9 +9,10 @@ export default function LoginPage() {
 
     const emailRef = useRef()
     const passwordRef = useRef()
+
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
-    //const history = useNavigate()
+    const history = useNavigate()
 
 //     async function handleSubmit(e) {
 //         e.preventDefault()
@@ -27,6 +28,34 @@ export default function LoginPage() {
 
 //     setLoading(false)
 //   }
+    
+const [loginError, setLoginError] = useState(false);
+const [loggedIn, setLoggedIn] = useState(false);
+
+const loginErrMsg= () => {
+    if(loggedIn === true) setLoggedIn(false)
+    setLoginError(true);
+  };
+const loginSucess = () => {
+    if(loginError === true) setLoginError(false)
+    setLoggedIn(true)
+}
+
+async function handleSubmit(e) {
+    e.preventDefault()
+    const email = "email"
+    const pass = "password"
+
+    if (passwordRef.current.value !== pass || emailRef.current.value !== email) {
+        loginErrMsg()
+    } else {
+        loginSucess()
+        // history.push("/")
+    } 
+    passwordRef.current.value = null
+    emailRef.current.value = null
+  }
+
 
     return (
         <>
@@ -35,13 +64,26 @@ export default function LoginPage() {
 
             <div className={styles.loginWrapper}>
                 <img className={styles.loginTitleImg} src={MainIconImg}/>
-                <h1 className={styles.loginTitle}></h1>
-                <form className={styles.loginForm}>
-                    <div><input type="text" placeholder='Username' ref={emailRef} autoComplete="new-password"/></div>
-                    <div style={{ marginTop: 10 }}><input type="password"placeholder='Password' ref={passwordRef}  autoComplete="new-password"/></div>
-                    <button type="submit" className={styles.loginBtn}>Login</button>
+                <h1 className={styles.loginTitle}>Code Theory</h1>
                 
+                    {loginError && (
+                        <div>
+                            <p className={styles.loginErrorMsg}>Invalid username and/or password</p>
+                        </div>
+                    )}
+                    {loggedIn && (
+                        <div>
+                            <p className={styles.loginSucessful}>Welcome Back!</p>
+                        </div>
+                    )}
+                    
+                <form className={styles.loginForm} onSubmit={handleSubmit}>
+                    <div><input type="text" placeholder='Email..' ref={emailRef} /></div>
+                    <div style={{ marginTop: 10 }}><input type="password"placeholder='Password..' ref={passwordRef}  /></div>
+                    <button type="submit" className={styles.loginBtn}>Login</button>
                 </form>
+                <div>or</div>
+                <h1 className={styles.signUpLink}>Sign Up</h1>
             </div>
 
         </div>
