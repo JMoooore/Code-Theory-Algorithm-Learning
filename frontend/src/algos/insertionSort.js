f5db242c2014527c0e1546fdfd19d6b6ea913e0f
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-02-15 10:12:52 -0500

    Fixed replit for insertion sort

--- frontend/src/algos/insertionSort.js
+++ frontend/src/algos/insertionSort.js
@@ -6,7 +6,7 @@ const algo = {}
 algo.name = 'Insertion Sort'
 algo.routeName = 'insertionSort'
 
-// algo.replitCode = "https://replit.com/@EmpatheticLions/Insertion-Sort?lite=true";
+algo.replitCode = "https://replit.com/@EmpatheticLions/Insertion-Sort?lite=true";
 
 algo.description = 'Insertion sort is a simple sorting algorithm that works similar to the way you sort playing cards in your hands. The array is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part.'
 
