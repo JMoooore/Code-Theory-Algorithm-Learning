2d3dca626c4805a81b28f0cd63d82eb02dd1129b
fernandocm9 <88690826+fernandocm9@users.noreply.github.com>
2022-02-14 19:42:17 -0600

    added inseritonSort to list

--- frontend/src/algos/index.js
+++ frontend/src/algos/index.js
@@ -6,7 +6,8 @@ import quickSort from './quickSort';
 
 export default {
     bubbleSort,
-    quickSort
+    quickSort,
+    insertionSort
 };
 
-export { bubbleSort, quickSort };
+export { bubbleSort, quickSort, insertionSort };
